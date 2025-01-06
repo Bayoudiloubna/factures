792015f815af61e4efc9c55b2ac27a63df3fb507
Bayoudiloubna <bayoudi2404loubna@gmail.com>
2025-01-06 16:29:49 +0100

    first commit

---
+++ src/main/java/com/example/factures1/Factures1Application.java
@@ -0,0 +1,13 @@
+package com.example.factures1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Factures1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Factures1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/factures1/Factures1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.factures1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Factures1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
